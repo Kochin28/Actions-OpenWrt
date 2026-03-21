934a50ab950e96692001a7065aa1f3808b544600
Kochin28 <khakis.mar@gmail.com>
2026-03-22 03:52:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-#
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
-# 1. 修改默認 IP 地址為 192.168.6.1
+# 1. 修改默認 IP 地址
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-# 2. 強制定義 CT3003 硬件目標配置 (確保編譯出正確的 bin 檔)
+# 2. 清理舊配置
+sed -i '/CONFIG_TARGET/d' .config
+sed -i '/CONFIG_PACKAGE_luci-app/d' .config
+
+# 3. 定義硬件目標 (CT3003)
 echo "CONFIG_TARGET_mediatek=y" >> .config
 echo "CONFIG_TARGET_mediatek_filogic=y" >> .config
 echo "CONFIG_TARGET_mediatek_filogic_DEVICE_cetron_ct3003=y" >> .config
 
-# 3. 選中 SSR-Plus 插件及其核心
+# 4. 選中 SSR Plus 及其核心
 echo "CONFIG_PACKAGE_luci-app-ssr-plus=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Xray=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_ChinaDNS_NG=y" >> .config
 
-# 4. 選中 PassWall2 插件及其核心
+# 5. 選中 PassWall 2 及其核心
 echo "CONFIG_PACKAGE_luci-app-passwall=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_SingBox=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Xray=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_SingBox=y" >> .config
 
-# 5. 設置介面語言為簡體中文
+# 6. 極致瘦身：刪除唔必要嘅佔位大戶
+echo "# CONFIG_PACKAGE_luci-app-docker is not set" >> .config
+echo "# CONFIG_PACKAGE_luci-app-adguardhome is not set" >> .config
+echo "# CONFIG_PACKAGE_luci-theme-bootstrap is not set" >> .config
+echo "# CONFIG_PACKAGE_luci-theme-material is not set" >> .config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config
