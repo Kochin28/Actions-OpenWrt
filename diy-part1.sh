db43ef9c167278f0039b37d9864ed18ec4036a01
Kochin28 <khakis.mar@gmail.com>
2026-03-22 03:59:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# 添加 PassWall 2 軟件源
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >> feeds.conf.default
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages;main' >> feeds.conf.default
-
-# 添加 SSR Plus 軟件源 (如果 EasyWrt 未內置)
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >> openwrt/feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages;main' >> openwrt/feeds.conf.default
