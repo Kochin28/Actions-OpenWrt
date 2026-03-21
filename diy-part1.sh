f8d1c550e5443df8fdce6c530d18a83995f3c825
Kochin28 <khakis.mar@gmail.com>
2026-03-22 03:53:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-
 # 添加 PassWall 2 軟件源
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >> feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages;main' >> feeds.conf.default
+
+# 添加 SSR Plus 軟件源 (如果 EasyWrt 未內置)
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
